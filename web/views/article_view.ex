7c5b2c36f052358e46a981a6ede6a2a5de87856f
Arian van Putten <aeroboy94@gmail.com>
2015-06-15 16:54:10 +0200

    Fix titles to be handled by the view.

--- web/views/article_view.ex
+++ web/views/article_view.ex
@@ -1,8 +1,21 @@
 defmodule Blog.ArticleView do
   use Blog.Web, :view
 
+  def render("title.index.html", assigns) do
+      "Articles"
+  end
 
-  def render("title.html", assigns) do
+  def render("title.new.html", assigns) do
+    "New article"
+  end
+
+  def render("title.edit.html", assigns) do
+    "Editing "<> assigns.article.title
+  end
+
+  def render("title.show.html", assigns) do
     assigns.article.title
   end
+
+
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,7 +1,3 @@
 defmodule Blog.LayoutView do
   use Blog.Web, :view
-
-  def title do
-    "euhm wat nu"
-  end
 end
